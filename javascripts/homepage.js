d92546e9c3195c2f0cfd50aa0b85e71275038d87
Mukesh Bist <bistfake1@gmail.com>
2026-06-27 12:21:40 +0545

    swiper_thumb of video_carousel completed

--- javascripts/homepage.js
+++ javascripts/homepage.js
@@ -1,10 +1,9 @@
-// Remove spaceBetween quotes; change 480 breakpoint to centeredSlides: true
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.feature_trips_swiper', {
   freeMode: false,
   slidesPerView: 1.2,
   centeredSlides: true,
   grabCursor: true,
-  spaceBetween: 10, 
+  spaceBetween: 10,
   pagination: { el: '.swiper-pagination' },
   navigation: {
     nextEl: '.swiper_next_button',
@@ -13,6 +12,47 @@ const swiper = new Swiper('.swiper', {
   breakpoints: {
     1024: { slidesPerView: 2, centeredSlides: false },
     768: { slidesPerView: 2.5, centeredSlides: false },
-    480: { slidesPerView: 2, centeredSlides: false, paceBetween: 20, } 
+    480: { slidesPerView: 2, centeredSlides: false, paceBetween: 20, }
   }
+});
+
+
+// ------------------------------------------video carousel here --------------------------------------
+// swiper-thumb
+const swiper_thumb = new Swiper(".swiper_thumb", {
+  navigation: {
+    prevEl: ".swiper_thumb_prev",
+    nextEl: ".swiper_thumb_next",
+  },
+  spaceBetween: 10,
+  slidesPerView: 1.1,
+  breakpoints: {
+    1024: {
+      slidesPerView: 3,
+    },
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    }
+  },
+
+});
+
+function button_position() {
+  const swiper_thumb_image = document.querySelector(".swiper_thumb .swiper-slide .slider_bgImage");
+  const swiper_thumb_overlay = document.querySelectorAll(".swiper_thumb .swiper-slide .overlay");
+  const imageHeight = swiper_thumb_image.getBoundingClientRect().height;
+  const swiper_thumb_button = document.querySelectorAll(".swiper_thumb .swiper-slide button");
+  swiper_thumb_button.forEach((button) => {
+    button.style.top = `${(imageHeight) / 2}px`
+  })
+  swiper_thumb_overlay.forEach((overlay)=>{
+    overlay.style.height=`${imageHeight}px`
+  })
+}
+button_position();
+let resizeTimer;
+window.addEventListener("resize", () => {
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(button_position, 5); // run after 100ms idle
 });
